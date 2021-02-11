eb3a188addba1d49de54348939c757b3b00882a7
Mikael Dahlgren <mikael.dahlgren@valtech.com>
2021-02-11 13:48:03 +0100

    styling

--- drink-front/src/components/drink.js
+++ drink-front/src/components/drink.js
@@ -6,16 +6,30 @@ const Drink = ({drinkData}) => {
 
   return (
     <div className="drinkContainer">
+      <div className="drinkHeroContainer">
         <img className="drinkImg" src={drinkData.strDrinkThumb} />
-      <h2>{drinkData.strDrink}</h2>
-      <h3>instructions</h3>
-      <p>{drinkData.strInstructions}</p>
-      <h4>ingredients</h4>
-      <ul>
-          <li>{drinkData.strIngredient1} <b>{drinkData.strMeasure1}</b></li>
-          { (drinkData.strIngredient2 !== null) ? <li>{drinkData.strIngredient2} <b>{drinkData.strMeasure2}</b></li> : null}
-          { (drinkData.strIngredient3 !== null) ? <li>{drinkData.strIngredient3} <b>{drinkData.strMeasure3}</b></li> : null}
-      </ul>
+        <h1>{drinkData.strDrink}</h1>
+      </div>
+      <div className="detailsContainer">
+        <div className="row left">
+
+          <h3 className="instructionsHeading">Instructions</h3>
+          <p className="instructions">{drinkData.strInstructions}</p>
+        </div>
+
+        <div className="row right">
+          <h3>Ingredients</h3>
+          <ul>
+              { (drinkData.strIngredient1 !== null) ? <li><b>{drinkData.strIngredient1}</b> <span>{drinkData.strMeasure1}</span></li> : null}
+              { (drinkData.strIngredient2 !== null) ? <li><b>{drinkData.strIngredient2}</b> <span>{drinkData.strMeasure2}</span></li> : null}
+              { (drinkData.strIngredient3 !== null) ? <li><b>{drinkData.strIngredient3}</b> <span>{drinkData.strMeasure3}</span></li> : null}
+              { (drinkData.strIngredient4 !== null) ? <li><b>{drinkData.strIngredient4}</b> <span>{drinkData.strMeasure4}</span></li> : null}
+              { (drinkData.strIngredient5 !== null) ? <li><b>{drinkData.strIngredient5}</b> <span>{drinkData.strMeasure5}</span></li> : null}
+              { (drinkData.strIngredient6 !== null) ? <li><b>{drinkData.strIngredient6}</b> <span>{drinkData.strMeasure6}</span></li> : null}
+          </ul>
+        </div>
+
+      </div>
     </div>
   )
 }
